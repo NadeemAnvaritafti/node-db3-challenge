02a83436c4c2f99dcd9f6f524fc40350b0d133d3
Nadeem Anvaritafti <nanvari@live.com>
2019-12-11 16:53:08 -0600

    added functions in scheme model for GET endpoints

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,17 +10,23 @@ module.exports = {
 }
 
 function find() {
-
+    return db('schemes');
 }
 
 
 function findById(id) {
-    
+    return db('schemes')
+        .where({ id })
+        .first();
 }
 
 
 function findSteps(id) {
-    
+    return db('steps')
+        .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .join('schemes', 'steps.scheme_id', '=', 'schemes.id')
+        .where('scheme_id', '=', id)
+        .orderBy('steps.step_number');
 }
 
 
